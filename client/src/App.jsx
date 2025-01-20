c0e78d9f148be75a593c4f8333796c29649793f9
vivek soni <sonivivek346@gmail.com>
2025-01-20 21:15:30 +0530

    added routes and created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Project from "./pages/Project";
 function App() {
   return (
     <>
-      <h1 className="bg-red-400">hello viviek</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+          <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+          <Route path="/project" element={<Project />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
-
