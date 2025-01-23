356be94b0c3a591773917e35be3b1df13f65a5ed
vivek soni <sonivivek346@gmail.com>
2025-01-23 16:42:32 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
 
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://sonivivek346:URtn9tJwe4rSWRoX@mernblog.wojfj.mongodb.net/?retryWrites=true&w=majority&appName=MernBlog"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000,()=>{console.log("Server is running on port 3000!")})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
